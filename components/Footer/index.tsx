597f11469926f614cac3f7135a80a8f1efcb96b3
Matheus Gomes <47065962+matheus369k@users.noreply.github.com>
2024-07-19 12:25:41 -0300

    create footer component

---
+++ components/Footer/index.tsx
@@ -0,0 +1,9 @@
+import { StyledFooter } from './styles'
+
+export function Footer() {
+    return (
+        <StyledFooter>
+            <p>Curso de Nextjs &copy; 2024</p>
+        </StyledFooter>
+    )
+}
---
+++ components/Footer/styles.ts
@@ -0,0 +1,12 @@
+import { styled } from 'styled-components'
+
+export const StyledFooter = styled.footer`
+    background: ${props => props.theme.black};
+    color: ${props => props.theme.white};
+
+    height: 4rem;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`
