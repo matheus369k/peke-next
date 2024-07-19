c1714b34ff3b656e6ebef0c0c77e73125a1c8cc6
Matheus Gomes <47065962+matheus369k@users.noreply.github.com>
2024-07-19 12:24:14 -0300

    create default layout component

---
+++ components/Layout/styles.ts
@@ -0,0 +1,8 @@
+import {styled} from 'styled-components'
+
+export const StyledMain = styled.main`
+    background: ${props => props.theme.white};
+    color: ${props => props.theme.black};
+
+    min-height: 100vh;
+` 
