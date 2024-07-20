ae75fe633ef16cb01d325e97ca74d980a9c1b4f8
Matheus Gomes <47065962+matheus369k@users.noreply.github.com>
2024-07-20 14:26:06 -0300

    update tsx file and create styles from page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,9 +1,18 @@
-import { NavBar } from "../../components/Navbar";
+'use client'
+
+import Image from "next/image";
+import { StyledAbout } from "./styles";
 
 export default function About() {
     return (
-        <>
-            <h1>about</h1>
-        </>
+        <StyledAbout>
+            <h1>Sobre o projeto</h1>
+
+            <p>
+                Nesta aula vamos criar uma página para o nosso sistema de #Next.js A ideia é relembrar este conceito de novas páginas utilizando este framework Para depois partirmos para próximas etapas 🔷 Telegram: https://t.me/horadecodar. Discord Hora de Codar:   / discord
+            </p>
+
+            <Image src='/charizard.png' alt='' width={300} height={300} />
+        </StyledAbout>
     )
 }
---
+++ app/about/styles.ts
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+export const StyledAbout = styled.div`
+    text-align: center;
+
+    h1 {
+        font-size: 2rem;
+            
+        margin-bottom: 1rem;
+    }
+
+    p {
+        margin: auto;
+        margin-bottom: 1.5rem;
+
+        max-width: 31.25rem;
+    }
+`
