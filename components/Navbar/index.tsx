7a291303c3c7ad0045a70e70427bcd3d2625ba2f
Matheus Gomes <47065962+matheus369k@users.noreply.github.com>
2024-07-20 14:22:59 -0300

    add navbar component and styles

--- components/Navbar/index.tsx
+++ components/Navbar/index.tsx
@@ -1,9 +1,17 @@
+'use client'
+
 import Link from 'next/link'
+import Image from 'next/image'
+
 import { StyledNav } from './styles'
 
 export function NavBar() {
     return (
         <StyledNav>
+            <div>
+                <Image src='/pokeball.png' alt='' loading='lazy' width={30} height={30} />
+                <h1>PokeNext</h1>
+            </div>
             <ul>
                 <li><Link href='/'>Home</Link></li>
                 <li><Link href='/about'>Sobre</Link></li>
--- components/Navbar/styles.ts
+++ components/Navbar/styles.ts
@@ -1,9 +1,20 @@
 import { styled } from 'styled-components'
 
 export const StyledNav = styled.nav`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    
     background: ${props => props.theme.black};
 
     padding: 1.5rem .875rem;
+    margin-bottom: 2rem;
+
+    div {
+        display: flex;
+        
+        gap: .5rem;
+    }
 
     ul {
         display: flex;
@@ -12,15 +23,40 @@ export const StyledNav = styled.nav`
         gap: 1rem;
 
         a {
+            position: relative;
+
             color: ${props => props.theme.white};
 
             text-transform: capitalize;
             font-weight: bold;
 
-            transition: .4s;
+            padding: .3125rem;
 
+            transition: .4s;
+            
             &:hover {
                 color: ${props => props.theme['gray-500']};
+            }
+
+            &::before {
+                content: '';
+
+                width: 0%;
+                height: 2px;
+
+                position: absolute;
+                left: 50%;
+                bottom: 0;
+
+                transform: translateX(-50%);
+
+                background: ${props => props.theme['gray-500']};
+
+                transition: width .2s;
+            }
+
+            &:hover::before {
+                width: 100%
             }
         }
     }
