2a1e3434043994e93694e9b6175c7b17ccc4b158
Matheus Gomes <47065962+matheus369k@users.noreply.github.com>
2024-07-22 14:08:43 -0300

    movi file

---
+++ app/styles.ts
@@ -0,0 +1,39 @@
+'use client'
+
+import styled from "styled-components";
+
+export const StyledHome = styled.div`
+    display: flex;
+    flex-direction: column;
+
+    gap: 2rem;
+
+    div {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+
+        gap: .4rem;
+
+        h1 {
+            color: ${props=> props.theme.red};
+
+            text-align: center;
+
+            font-size: 3rem;
+
+            span {
+                color: ${props=> props.theme.black};
+            }
+        }
+    }
+
+
+    ul {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+
+        flex-wrap: wrap;
+    }
+`
